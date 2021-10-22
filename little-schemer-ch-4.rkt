605ab3715909e861d5c191bc1dda5157ef004d9d
Jesse Korpela <donburij@gmail.com>
2021-10-22 21:14:35 +0300

    continue chapter 4

--- little-schemer-ch-4.rkt
+++ little-schemer-ch-4.rkt
@@ -14,9 +14,30 @@
   (λ (tup)
     (cond
       [(null? tup) 0]
-      [else (+ (car tup) (addtup (cdr tup)))])))
+      [else (+ (car tup) (addtup (cdr tup)))]
+      )))
 
 
 (check-expect (addtup '(1 2 3)) 6)
+
+(define ×
+  (λ (n m)
+    (cond
+      [(zero? m) 0]
+      [else (+ n (× n (sub1 m)))]
+      )))
+
+(check-expect (× 4 5) 20)
+(check-expect (× 12 3) 36)
+
+(define tup+
+  (λ (tup1 tup2)
+    (cond
+      [(and (null? tup1) (null? tup2)) '()]
+      [else (cons (+ (car tup1) (car tup2)) (tup+ (cdr tup1) (cdr tup2)))]
+      )))
+
+(check-expect (tup+ '(3 6 9 11 4) '(8 5 2 0 7)) '(11 11 11 11 11))
+(check-expect (tup+ '(2 3) '(4 6)) '(6 9))
 
 (test)
